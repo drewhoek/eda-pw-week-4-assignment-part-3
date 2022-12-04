cea2c66824ca7aea5d0dd61b6a5466c8a9952557
Drew Hoek <drewh815@gmail.com>
2022-12-03 22:38:12 -0600

    Created isFull function, addItems2 function and tested

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,7 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = []
+const maxItems = 5
 
 function addItem( item ) {
     console.log( 'in addItem' );
@@ -29,3 +30,38 @@ function empty() {
 console.log( 'Basket should now be empty (expect true),', empty(), 'Basket is:', basket );
 basket.push( 'test_thing' );
 console.log( 'Basket should now only contain a test_thing', basket );
+
+function isFull() {
+    console.log( 'in isFull' );
+    if ( basket.length < maxItems ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+console.log( 'Expect function to return false now:', isFull() );
+basket.push( 'bananas', 'bread', 'oranges', 'pop' ); // To test if function is working with 5 items.
+console.log( basket );
+console.log ( 'Expect function to return true now:', isFull() );
+
+function addItem2( item ) {
+    console.log( 'in addItem2' );
+    basket.push( item );
+    if ( isFull() === false ) {
+        return 'You can still add more to the basket! You have: ' + basket.length + ' items.';
+    } else {
+        basket.pop();
+        return 'The basket is full! You cannot add more items.'
+    }
+}
+
+console.log( addItem2( 'water' ) );
+console.log( basket );
+basket.pop();
+basket.pop();
+basket.pop();
+console.log( addItem2( 'chicken' ) );
+// Need to add items one at a time for function to work.
+// Is there a way to put multiple item into the basket at once and -
+// for the function to behave properly when doing so?
+
